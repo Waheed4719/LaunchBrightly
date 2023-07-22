00d340371a8cf27b04e22e67848d201c2a901069
Waheed Uddin Ahmed <dmc4719@gmail.com>
2023-07-22 06:42:00 +0600

    Fix Searchbar close icon layout issue

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex items-center w-[250px]">
+  <div class="flex items-center w-[250px] relative">
     <input
       placeholder="Search by Name"
       v-model="searchText"
       class="px-3 py-2 pr-[15px] w-full bg-gray-100 text-sm outline-none border-2 border-transparent rounded-full focus:border-blue-500 transition-all duration-150" />
     <CloseIcon
-      class="absolute right-10 text-gray-400 cursor-pointer"
+      class="absolute right-[5px] text-gray-400 cursor-pointer"
       @click="searchText = ''" />
   </div>
 </template>
