68a8d6fc1c7e5d3fc7dc865834b10101eb707e8a
Waheed Uddin Ahmed <dmc4719@gmail.com>
2023-07-22 07:56:57 +0600

    Update pagination component cursor on disabled buttons

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -4,12 +4,15 @@
       v-if="numberOfPages >= 1"
       class="bg-white m-0 p-10 flex gap-[5px] items-center border-none box-border overflow-hidden break-words content-center rounded-14">
       <li
-        class="flex cursor-pointer mb-0 select-none"
         aria-label="go to previous page"
         @click="previous()"
-        :class="{
-          'cursor-not-allowed': currentPage === 1,
-        }">
+        :class="[
+          {
+            'cursor-not-allowed': currentPage === 1,
+            'cursor-pointer': currentPage !== 1,
+          },
+          'flex mb-0 select-none',
+        ]">
         <div
           class="rounded-md text-sm font-bold px-[15px] py-[10px] hover:text-[#333333] hover:bg-[#e9e9e9] :disabled:bg-[#f9fafb]">
           &laquo;
@@ -31,10 +34,14 @@
         </div>
       </li>
       <li
-        class="flex cursor-pointer mb-0 select-none"
-        :class="{
-          'cursor-not-allowed': currentPage === numberOfPages || !numberOfPages,
-        }"
+        :class="[
+          {
+            'cursor-not-allowed':
+              currentPage === numberOfPages || !numberOfPages,
+            'cursor-pointer': currentPage !== numberOfPages,
+          },
+          'flex mb-0 select-none',
+        ]"
         aria-label="go to next page"
         @click="next()">
         <div
@@ -76,6 +83,7 @@ const setCurrentPage = (number: number) => {
 };
 
 const previous = () => {
+  console.log(currentPage.value);
   if (currentPage.value === 1) return;
   emit('navigate', currentPage.value - 1);
 };
